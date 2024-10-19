8b9406c4a843e5d49243b537a1df184632b5f4ae
Harsh Devisha <harshdevisha9111@gmail.com>
2024-10-18 18:26:08 -0700

    (feat) server.

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,19 @@
 const std = @import("std");
+const net = std.net;
 const print = std.debug.print;
 
 pub fn main() !void {
-    print("Hello, world!\n");
+    const addr = try net.Address.parseIp("127.0.0.1", 6379);
+
+    var server: net.Server = try net.Address.listen(addr, .{});
+    defer server.deinit();
+
+    const conn: net.Server.Connection = try server.accept();
+
+    while (true) {
+        var buf: [4096]u8 = undefined;
+        const n = try conn.stream.read(buf[0..]);
+        if (n == 0) break;
+        _ = try conn.stream.write(buf[0..n]);
+    }
 }
