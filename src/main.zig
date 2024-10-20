e682d5f36ed748856ee005dd3a0fbeed10a5470e
Harsh Devisha <harshdevisha9111@gmail.com>
2024-10-19 20:16:58 -0700

    (refactor) made code more readable

--- src/main.zig
+++ src/main.zig
@@ -3,18 +3,30 @@ const net = std.net;
 const mem = std.mem;
 const print = std.debug.print;
 
+fn readFromStream(conn: net.Server.Connection, buf: *[512]u8) !usize {
+    return try conn.stream.read(buf);
+}
+
+fn tokenizeReq(buf: *[512]u8) mem.TokenIterator(u8, .sequence) {
+    return mem.tokenizeSequence(u8, buf, "\r\n");
+}
+
 pub fn handleClient(conn: net.Server.Connection) !void {
     defer conn.stream.close();
     while (true) {
-        var buf: [512]u8 = undefined;
-        const n = try conn.stream.read(&buf);
-        if (n == 0) break;
-        var tokens: mem.TokenIterator(u8, .sequence) = mem.tokenizeSequence(u8, &buf, "\r\n");
+        var req: [512]u8 = undefined;
+        const bytesRead = try readFromStream(conn, &req);
+        if (bytesRead == 0) break;
+        var tokens: mem.TokenIterator(u8, .sequence) = tokenizeReq(&req);
         while (tokens.next()) |token| {
             print("token: {s}\n", .{token});
         }
         _ = try conn.stream.write("OK\r\n");
     }
+}
+
+pub fn listenForClient(server: *net.Server) !net.Server.Connection {
+    return try server.*.accept();
 }
 
 pub fn main() !void {
@@ -24,8 +36,8 @@ pub fn main() !void {
     defer server.deinit();
 
     while (true) {
-        const conn: net.Server.Connection = try server.accept();
-        const thread = try std.Thread.spawn(.{}, handleClient, .{conn});
+        const clientConn: net.Server.Connection = try listenForClient(&server);
+        const thread = try std.Thread.spawn(.{}, handleClient, .{clientConn});
         thread.detach();
     }
 }
