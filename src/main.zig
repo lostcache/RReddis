d70324aa239a245f496ebb7dc2158a5add3d22e7
Harsh Devisha <harshdevisha9111@gmail.com>
2024-10-19 21:08:24 -0700

    (feat) parsing header

--- src/main.zig
+++ src/main.zig
@@ -11,17 +11,31 @@ fn tokenizeReq(buf: *[512]u8) mem.TokenIterator(u8, .sequence) {
     return mem.tokenizeSequence(u8, buf, "\r\n");
 }
 
+const HeaderParseError = error{ Overflow, InvalidCharacter, MissingHeader, HeaderParseError };
+fn parseHeader(maybeToken: ?[]const u8) HeaderParseError!usize {
+    if (maybeToken == null) return error.MissingHeader;
+    const token = maybeToken.?;
+    if (mem.startsWith(u8, token, "*") == false) return error.HeaderParseError;
+    return try std.fmt.parseInt(u8, token[1..], 10);
+}
+
+fn handleRequest(req: *[512]u8) ![]const u8 {
+    var tokens: mem.TokenIterator(u8, .sequence) = tokenizeReq(req);
+    const cmdLen = try parseHeader(tokens.next());
+    const tokenLen = cmdLen * 2;
+    _ = tokenLen;
+    return "OK\r\n";
+}
+
 pub fn handleClient(conn: net.Server.Connection) !void {
     defer conn.stream.close();
     while (true) {
         var req: [512]u8 = undefined;
         const bytesRead = try readFromStream(conn, &req);
         if (bytesRead == 0) break;
-        var tokens: mem.TokenIterator(u8, .sequence) = tokenizeReq(&req);
-        while (tokens.next()) |token| {
-            print("token: {s}\n", .{token});
-        }
-        _ = try conn.stream.write("OK\r\n");
+        const res = try handleRequest(&req);
+        const resBytes = try conn.stream.write(res);
+        print("Served {d} bytes\nRes: {s}", .{ resBytes, res });
     }
 }
 
@@ -40,4 +54,16 @@ pub fn main() !void {
         const thread = try std.Thread.spawn(.{}, handleClient, .{clientConn});
         thread.detach();
     }
+}
+
+test "test parseHeader" {
+    const t = std.testing;
+
+    try t.expectError(HeaderParseError.MissingHeader, parseHeader(null));
+
+    try t.expectError(HeaderParseError.HeaderParseError, parseHeader("abc"));
+
+    try t.expectEqual(123, parseHeader("*123"));
+
+    try t.expectError(HeaderParseError.InvalidCharacter, parseHeader("*abc"));
 }
