2dd300b0a2da4ca9df54dc5ac6a9376f06874c7a
Harsh Devisha <harshdevisha9111@gmail.com>
2024-10-20 21:45:21 -0700

    (feat) concurrent echo

--- src/main.zig
+++ src/main.zig
@@ -19,27 +19,72 @@ fn parseHeader(maybeToken: ?[]const u8) HeaderParseError!usize {
     return try std.fmt.parseInt(u8, token[1..], 10);
 }
 
-fn handleRequest(req: *[512]u8) ![]const u8 {
-    var tokens: mem.TokenIterator(u8, .sequence) = tokenizeReq(req);
-    const cmdLen = try parseHeader(tokens.next());
-    const tokenLen = cmdLen * 2;
-    _ = tokenLen;
-    return "OK\r\n";
+const CommandParseError = error{ InvalidCommand, Overflow, InvalidCharacter };
+fn getCmdLen(cmdHeader: []const u8) CommandParseError!usize {
+    if (mem.startsWith(u8, cmdHeader, "$") == false) return error.InvalidCommand;
+    return try std.fmt.parseInt(u8, cmdHeader[1..], 10);
 }
 
-pub fn handleClient(conn: net.Server.Connection) !void {
+fn getNextToken(tokens: *mem.TokenIterator(u8, .sequence), processedTokens: *usize, tokenCount: *const usize) ![]const u8 {
+    const maybeToken = tokens.next();
+    if (maybeToken == null and processedTokens.* < tokenCount.*) return error.InvalidRequest;
+    processedTokens.* += 1;
+    return maybeToken.?;
+}
+
+fn checkTokenLen(token: []const u8, cmdLen: usize) CommandParseError!void {
+    if (token.len != cmdLen) {
+        return error.InvalidCommand;
+    }
+    return;
+}
+
+fn getResponse(tokens: *mem.TokenIterator(u8, .sequence), tokenCount: *const usize) ![]const u8 {
+    var processedTokens: usize = 0;
+    while (processedTokens < tokenCount.*) {
+        var headerToken: []const u8 = try getNextToken(tokens, &processedTokens, tokenCount);
+        var tokenLen: usize = try getCmdLen(headerToken);
+        var token: []const u8 = try getNextToken(tokens, &processedTokens, tokenCount);
+        try checkTokenLen(token, tokenLen);
+        if (std.ascii.eqlIgnoreCase(token, "PING")) {
+            return "PONG";
+        } else if (std.ascii.eqlIgnoreCase(token, "ECHO")) {
+            headerToken = try getNextToken(tokens, &processedTokens, tokenCount);
+            tokenLen = try getCmdLen(headerToken);
+            token = try getNextToken(tokens, &processedTokens, tokenCount);
+            try checkTokenLen(token, tokenLen);
+            return token;
+        }
+    }
+    return "-ERROR\r\n";
+}
+
+const RequestParseError = HeaderParseError || error{InvalidRequest} || CommandParseError;
+fn handleRequest(req: *[512]u8) RequestParseError![]const u8 {
+    var tokens: mem.TokenIterator(u8, .sequence) = tokenizeReq(req);
+    const cmdCount = try parseHeader(tokens.next());
+    const tokenCount = cmdCount * 2;
+    return getResponse(&tokens, &tokenCount) catch return "-ERROR\r\n";
+}
+
+fn handleClient(conn: net.Server.Connection) !void {
     defer conn.stream.close();
     while (true) {
         var req: [512]u8 = undefined;
         const bytesRead = try readFromStream(conn, &req);
         if (bytesRead == 0) break;
-        const res = try handleRequest(&req);
-        const resBytes = try conn.stream.write(res);
-        print("Served {d} bytes\nRes: {s}", .{ resBytes, res });
+        const res = handleRequest(&req) catch |err| {
+            print("Error: {any}", .{err});
+            _ = try conn.stream.write("-ERROR\r\n");
+            return;
+        };
+        try std.fmt.format(conn.stream.writer(), "${d}\r\n{s}\r\n", .{ res.len, res });
+        print("Res: {s}\n", .{res});
     }
+    print("client handled, closing stream\n", .{});
 }
 
-pub fn listenForClient(server: *net.Server) !net.Server.Connection {
+fn listenForClient(server: *net.Server) !net.Server.Connection {
     return try server.*.accept();
 }
 
@@ -54,16 +99,13 @@ pub fn main() !void {
         const thread = try std.Thread.spawn(.{}, handleClient, .{clientConn});
         thread.detach();
     }
+    print("shutting server gracefully\n", .{});
 }
 
 test "test parseHeader" {
     const t = std.testing;
-
     try t.expectError(HeaderParseError.MissingHeader, parseHeader(null));
-
     try t.expectError(HeaderParseError.HeaderParseError, parseHeader("abc"));
-
     try t.expectEqual(123, parseHeader("*123"));
-
     try t.expectError(HeaderParseError.InvalidCharacter, parseHeader("*abc"));
 }
