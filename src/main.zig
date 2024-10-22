e2071dc02117fa655d7b080ea4c6782ac55e8099
Harsh Devisha <harshdevisha9111@gmail.com>
2024-10-21 20:17:16 -0700

    (feat) GET and SET

--- src/main.zig
+++ src/main.zig
@@ -39,7 +39,7 @@ fn checkTokenLen(token: []const u8, cmdLen: usize) CommandParseError!void {
     return;
 }
 
-fn getResponse(tokens: *mem.TokenIterator(u8, .sequence), tokenCount: *const usize) ![]const u8 {
+fn getResponse(tokens: *mem.TokenIterator(u8, .sequence), tokenCount: *const usize, map: *std.StringHashMap([]const u8)) ![]const u8 {
     var processedTokens: usize = 0;
     while (processedTokens < tokenCount.*) {
         var headerToken: []const u8 = try getNextToken(tokens, &processedTokens, tokenCount);
@@ -54,26 +54,49 @@ fn getResponse(tokens: *mem.TokenIterator(u8, .sequence), tokenCount: *const usi
             token = try getNextToken(tokens, &processedTokens, tokenCount);
             try checkTokenLen(token, tokenLen);
             return token;
+        } else if (std.ascii.eqlIgnoreCase(token, "SET")) {
+            const keyHeader = try getNextToken(tokens, &processedTokens, tokenCount);
+            const keyLen = try getCmdLen(keyHeader);
+            const key = try getNextToken(tokens, &processedTokens, tokenCount);
+            try checkTokenLen(key, keyLen);
+            const valHeader = try getNextToken(tokens, &processedTokens, tokenCount);
+            const valLen = try getCmdLen(valHeader);
+            const val = try getNextToken(tokens, &processedTokens, tokenCount);
+            try checkTokenLen(val, valLen);
+            map.put(key, val) catch return "-ERROR\r\n";
+            return "+OK";
+        } else if (std.ascii.eqlIgnoreCase(token, "GET")) {
+            const keyHeader = try getNextToken(tokens, &processedTokens, tokenCount);
+            const keyLen = try getCmdLen(keyHeader);
+            const key = try getNextToken(tokens, &processedTokens, tokenCount);
+            try checkTokenLen(key, keyLen);
+            const maybeVal = map.get(key);
+            if (maybeVal == null) {
+                return "$-1";
+            }
+            return maybeVal.?;
         }
     }
     return "-ERROR\r\n";
 }
 
 const RequestParseError = HeaderParseError || error{InvalidRequest} || CommandParseError;
-fn handleRequest(req: *[512]u8) RequestParseError![]const u8 {
+fn handleRequest(req: *[512]u8, map: *std.StringHashMap([]const u8)) RequestParseError![]const u8 {
     var tokens: mem.TokenIterator(u8, .sequence) = tokenizeReq(req);
     const cmdCount = try parseHeader(tokens.next());
     const tokenCount = cmdCount * 2;
-    return getResponse(&tokens, &tokenCount) catch return "-ERROR\r\n";
+    return getResponse(&tokens, &tokenCount, map) catch return "-ERROR\r\n";
 }
 
-fn handleClient(conn: net.Server.Connection) !void {
+fn handleClient(conn: net.Server.Connection, alloc: *mem.Allocator) !void {
     defer conn.stream.close();
+    var map = std.StringHashMap([]const u8).init(alloc.*);
+    defer map.deinit();
     while (true) {
         var req: [512]u8 = undefined;
         const bytesRead = try readFromStream(conn, &req);
         if (bytesRead == 0) break;
-        const res = handleRequest(&req) catch |err| {
+        const res = handleRequest(&req, &map) catch |err| {
             print("Error: {any}", .{err});
             _ = try conn.stream.write("-ERROR\r\n");
             return;
@@ -89,6 +112,10 @@ fn listenForClient(server: *net.Server) !net.Server.Connection {
 }
 
 pub fn main() !void {
+    var arena_alloc = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var alloc = arena_alloc.allocator();
+    defer arena_alloc.deinit();
+
     const addr = try net.Address.parseIp("127.0.0.1", 6379);
 
     var server: net.Server = try net.Address.listen(addr, .{ .reuse_address = true });
@@ -96,10 +123,9 @@ pub fn main() !void {
 
     while (true) {
         const clientConn: net.Server.Connection = try listenForClient(&server);
-        const thread = try std.Thread.spawn(.{}, handleClient, .{clientConn});
+        const thread = try std.Thread.spawn(.{}, handleClient, .{ clientConn, &alloc });
         thread.detach();
     }
-    print("shutting server gracefully\n", .{});
 }
 
 test "test parseHeader" {
